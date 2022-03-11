a43e0cfb39eedc128e724dcf611d7531f5871109
arsen1c <vector2912@gmail.com>
2022-03-11 20:50:04 +0530

    🐛fix: more comments and naming improvements

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -1,3 +1,13 @@
+// String template for specific command.
+const specificCommandTemplate = (name, description, args, usage) => {
+  return (
+    `*Command name:*\n${name}\n\n` +
+      `*Description:*\n${description}\n\n` +
+      `*Requires Arguments:*\n${args}\n\n` +
+      `*Usage:*\n\/${name} ${usage}\n\n` 
+  );
+}  
+
 export default {
   name: 'help',
   description: 'help!',
@@ -5,22 +15,22 @@ export default {
   usage: '<command-name>',
   chatAction: 'typing',
   execute(ctx, args, commands) {
-    if (args && commands.has(args[0])) {
-      const { default: command } = commands.get(args[0]);
-      return ctx.replyWithMarkdown(
-        `*Command name:*\n${command.name}\n\n` +
-          `*Description:*\n${command.description}\n\n` +
-          `*Requires Arguments:*\n${command.args}\n\n` +
-          `*Usage:*\n\/${command.name} ${command.usage}\n\n`,
+    if (args && commands.has(args[0])) {    // check if args are passed and commands collection has the command.
+      const { default: command } = commands.get(args[0]);   // get the command. 
+      
+      return ctx.replyWithMarkdown(         // display information about that particular command.
+        specificCommandTemplate(command.name, command.description, command.args, command.usage),
         { reply_to_message_id: ctx.message.message_id }
       );
-    } else if (!args.length) {
-      let command = 'Available Commands:\n';
-      for (let [value, _] of commands) {
-        command += `/${value}\n`;
+
+    } else if (!args.length) {    // if no arguments are passed, show all the available commands.
+      let commandsMessage = 'Available Commands:\n';  
+
+      for (let [value, _] of commands) {  // add all the commands from commands collection.
+        commandsMessage += `/${value}\n`;
       }
-  command += "\nType /help <command_name> for command details."
-      ctx.reply(command);
+      commandsMessage += "\nType /help <command_name> for command details."
+      ctx.reply(commandsMessage);
     } else {
       ctx.reply('No command found');
     }
